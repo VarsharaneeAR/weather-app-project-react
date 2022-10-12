9ef2c62159f8e4e8a59110434bd3a4ef8de3a0fc
VarsharaneeAR <100801262+VarsharaneeAR@users.noreply.github.com>
2022-10-12 17:31:50 +0400

    Added link to Github and Profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Weather Application</h1>
+      This project is created by {""}<a href="https://www.shecodes.io/graduates/21150-varsharanee-roopun" target="_blank" rel="noreferrer">VarsharaneeAR </a>and is 
+      <footer> <a href="https://github.com/VarsharaneeAR/weather-app-project-react" target="_blank" rel="noreferrer">is open shources on Github</a></footer>
+     
     </div>
-  );
+  ); 
 }
-
-export default App;
