56bfbdfc5ea1ee0cff03b7d5948c56d03e025561
VarsharaneeAR <100801262+VarsharaneeAR@users.noreply.github.com>
2022-10-16 15:05:45 +0400

    updates rel according to audit suggestion

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
       <Weather defaultCity="Harare"/>
       <footer> 
         <p>
-      This project is created by {""}<a href="https://www.shecodes.io/graduates/21150-varsharanee-roopun" target="_blank" className='link-light' rel="noreferrer">VarsharaneeAR </a>and {""}
-      <a href="https://github.com/VarsharaneeAR/weather-app-project-react" target="_blank" className="link-light" rel="noreferrer">{""}is open sourced on Github</a></p></footer>
+      This project is created by {""}<a href="https://www.shecodes.io/graduates/21150-varsharanee-roopun" target="_blank" className='link-light' rel="noreferrer"> VarsharaneeAR </a>and {""}
+      <a href="https://github.com/VarsharaneeAR/weather-app-project-react" target="_blank" className="link-light" rel="noopener noreferrer">{""} is open sourced on Github</a></p></footer>
      
     </div>
   ); 
