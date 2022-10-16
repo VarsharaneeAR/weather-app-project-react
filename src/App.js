f58e43952fb3548c72d75f5b30044c1666a5d461
VarsharaneeAR <100801262+VarsharaneeAR@users.noreply.github.com>
2022-10-16 14:07:08 +0400

    styling changes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Weather from './Weather';
 export default function App() {
   return (
     <div className="App">
-
-      
       <Weather defaultCity="Harare"/>
       <footer> 
         <p>
-      This project is created by {""}<a href="https://www.shecodes.io/graduates/21150-varsharanee-roopun" target="_blank" className='link-secondary' rel="noreferrer">VarsharaneeAR </a>and {""}
-      <a href="https://github.com/VarsharaneeAR/weather-app-project-react" target="_blank" className='link-secondary' rel="noreferrer">{""}is open sourced on Github</a></p></footer>
+      This project is created by {""}<a href="https://www.shecodes.io/graduates/21150-varsharanee-roopun" target="_blank" className='link-light' rel="noreferrer">VarsharaneeAR </a>and {""}
+      <a href="https://github.com/VarsharaneeAR/weather-app-project-react" target="_blank" className="link-light" rel="noreferrer">{""}is open sourced on Github</a></p></footer>
      
     </div>
   ); 
+
+  
 }
